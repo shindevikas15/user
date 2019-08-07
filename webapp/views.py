60817c07b23ae1722ce52ddfb9182b2f36c89d4a
Puneet <puneetpilania13@gmail.com>
2019-08-07 10:19:50 +0530

    firstcommit

---
+++ webapp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from webapp.forms import signupForm
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+def home_page(request):
+    return render(request,'myapp/home.html')
+
+
+@login_required
+def study_material(request):
+    return render(request,'myapp/study_material.html')
+
+
+def logout(request):
+    return render(request,'myapp/logout.html')
+
+def registration_view(request):
+    form=signupForm()
+    if request.method=='POST':
+        form=signupForm(request.POST)
+        user=form.save(commit=True)
+        user.set_password(user.password)
+        user.save()
+
+        return HttpResponseRedirect('/accounts/login')
+    return render(request,"myapp/registration.html",{'form':form})
